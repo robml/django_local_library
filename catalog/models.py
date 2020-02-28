eca007c2f9805da6ebd6f7e3893e1ac7ef4d8da6
robml <robjrm@gmail.com>
2020-02-29 03:11:33 +0400

    Applying a few corrections to displayed labels

--- catalog/models.py
+++ catalog/models.py
@@ -16,7 +16,7 @@ class Genre(models.Model):
 
 class Language(models.Model):
     """Model representing a Language (e.g. English, French, Japanese, etc.)"""
-    name = models.CharField(max_length=200, help_text="Enter the book's natural language (e.g. English, French, Japanese, etc.")
+    name = models.CharField(max_length=200, help_text="Enter the book's natural language (e.g. English, French, Japanese, etc.)")
 
     def __str__(self):
         """String for representing the Model object (in Admin site etc.)"""
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'e3dlu=f4%830h*h7n4z62t-fvx6+wv
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['float-local-lib.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['float-local-lib.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
